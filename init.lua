40e28ea618b681b9e74b66ad400cc6c4f656b779
Maciej Małecki <me@mmalecki.com>
2023-12-09 18:30:29 +0100

    Use `cR` to trigger LSP rename

--- init.lua
+++ init.lua
@@ -102,6 +102,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'cR', vim.lsp.buf.rename, bufopts)
   end
 })
 
