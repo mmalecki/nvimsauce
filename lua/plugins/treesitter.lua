21ecb052a8ad6b210feec09e992eebb6c363f1ab
Maciej Małecki <me@mmalecki.com>
2026-05-16 15:42:13 +0200

    Update Treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,13 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ":TSUpdate",
+  lazy = false,
   config = function()
-    require('nvim-treesitter.config').setup {
-      ensure_installed = { "c", "lua", "rust", "python", "terraform", "cue", "typescript", "scss", "svelte" },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true
-      }
-    }
+    require('nvim-treesitter.config').setup { }
+    require('nvim-treesitter').install { "c", "lua", "rust", "python", "terraform", "cue", "typescript", "scss", "svelte", "javascript", "html", "jsx" }
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { '*' },
+      callback = function() vim.treesitter.start() end,
+    })
   end,
 }
